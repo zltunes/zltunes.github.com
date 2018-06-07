b9c8cbe1289cc7ea3aeca82e4a87c40908c61954
tuneszhao <tuneszhao@tencent.com>
2018-06-07 16:31:00 +0800

    Site updated: 2018-06-07 16:30:59

--- js/code-number.js
+++ js/code-number.js
@@ -1,9 +1,14 @@
 $(function() {
   hljs.initHighlighting();
   $("code").each(function(){
-    $(this).html("<ol><li>" + $(this).html().replace(/\n/g,"\n</li><li>") +"\n</li></ol>");
+    if ($(this).text().split("\n").length > 1) {
+      $(this).html("<ol><li>" + $(this).html().replace(/\n/g,"\n</li><li>") +"\n</li></ol>");
+    }
   });
   $("code").each(function(){
     $(this).html($(this).html().replace("<li>\n</li></ol>","</ol>"));
   });
 });
+
+
+
